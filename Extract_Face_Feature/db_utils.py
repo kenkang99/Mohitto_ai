7fce1ba9daefd41cf47c10fa59b55f88341ca24f
SeonyeolJeong <wjdtjsduf1@dgu.ac.kr>
2025-05-18 18:55:13 +0900

    Modify: db_utils in CV Model, post data on DB

--- Extract_Face_Feature/db_utils.py
+++ Extract_Face_Feature/db_utils.py
@@ -1,7 +1,10 @@
+# Extract_Face_Feature/db_utils.py
 import pymysql
+from datetime import datetime
 from rds_config import rds_config
 
-def get_latest_request():
+# 가장 최근 1개의 request를 가져옴
+def get_latest_request(user_id: int, request_id: int):
     conn = pymysql.connect(
         host=rds_config['host'],
         user=rds_config['user'],
@@ -13,8 +16,44 @@ def get_latest_request():
     )
     try:
         with conn.cursor() as cursor:
-            sql = "SELECT * FROM request_table ORDER BY created_at DESC LIMIT 1"
-            cursor.execute(sql)
+            # request ID와 user ID가 일치하는 사진만 가져옴: 추후 api서버에서 user id와 request id를 모델로 송신예정
+            sql = """
+                SELECT * FROM request_table
+                WHERE user_id = %s AND request_id = %s
+                LIMIT 1
+            """
+            cursor.execute(sql, (user_id, request_id))
             return cursor.fetchone()
+    finally:
+        conn.close()
+
+# DB에 업로드
+def save_result_to_db(result: dict, request_id: int):
+    conn = pymysql.connect(
+        host=rds_config['host'],
+        user=rds_config['user'],
+        password=rds_config['password'],
+        database=rds_config['database'],
+        port=rds_config['port'],
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    try:
+        with conn.cursor() as cursor:
+            sql = """
+                INSERT INTO result_table (
+                    face_type, skin_tone, forehead, sex, created_at, request_id
+                )
+                VALUES (%s, %s, %s, %s, %s, %s)
+            """
+            cursor.execute(sql, (
+                result.get("얼굴형"),
+                result.get("피부색"),
+                result.get("이마 모양"),
+                result.get("성별", "unknown"),
+                datetime.now(),
+                request_id
+            ))
+        conn.commit()
     finally:
         conn.close()
