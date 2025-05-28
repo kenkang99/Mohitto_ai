268fa2b97f5992148362d6c3d229d5a8b93d6763
kenkang99 <ihkangg3@gmail.com>
2025-05-28 23:35:11 +0900

    Delete BackEnd directory

--- BackEnd/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .user import *
-from .request import *
-from .result import *
-from .hairstyle import *
-from .hair_recommendation import *
-from .hairshop import *
-from .hairshop_recommendation import *
